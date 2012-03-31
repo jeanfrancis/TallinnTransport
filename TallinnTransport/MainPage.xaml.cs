e8cc11af7f4269447ef179ad0afe99ef0d08697e
unknown <danar@MSA3644472.europe.corp.microsoft.com>
2012-03-31 14:38:05 +0300

    From main screen it goes to second screens(buss, troll, tramm, rong)

---
+++ TallinnTransport/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TallinnTransport
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+		
+        private void Troll_button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/Troll.xaml", UriKind.Relative));
+        }
+
+        private void Buss_button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/Buss.xaml", UriKind.Relative));
+        }
+
+        private void Tramm_button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/Tramm.xaml", UriKind.Relative));
+        }
+
+        private void Rong_button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/Rong.xaml", UriKind.Relative));
+        }
+    }
+}
